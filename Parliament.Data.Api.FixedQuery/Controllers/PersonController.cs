31445a755fef3437cd4b65539fab975a25927d1f
christopheralcock <christopher.alcock@gmail.com>
2017-03-21 12:08:21 +0000

    adds css

--- Parliament.Data.Api.FixedQuery/Controllers/PersonController.cs
+++ Parliament.Data.Api.FixedQuery/Controllers/PersonController.cs
@@ -15,17 +15,22 @@
         {
             var queryString = @"
 PREFIX : <http://id.ukpds.org/schema/>
-
+     
 CONSTRUCT {
     ?person
       	a :Person ;
-        :personGivenName ?givenName ;
-        :personFamilyName ?familyName .
+       	:personGivenName ?givenName ;
+       	:personFamilyName ?familyName ;
+        <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs ;
+        <http://example.com/A5EE13ABE03C4D3A8F1A274F57097B6C> ?listAs .
     }
 WHERE {
-    ?person a :Person .
-    OPTIONAL { ?person :personGivenName ?givenName } .
-    OPTIONAL { ?person :personFamilyName ?familyName } .
+    ?person 
+        a :Person .
+        OPTIONAL { ?person :personGivenName ?givenName } .
+        OPTIONAL { ?person :personFamilyName ?familyName } .
+        OPTIONAL { ?person <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs } .
+        ?person <http://example.com/A5EE13ABE03C4D3A8F1A274F57097B6C> ?listAs .
 }
 ";
 
