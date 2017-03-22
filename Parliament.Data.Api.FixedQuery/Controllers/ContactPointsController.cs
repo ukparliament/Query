540d435899db46d36602b5f50b7345486df82469
christopheralcock <christopher.alcock@gmail.com>
2017-03-22 12:27:18 +0000

    contact points queries identical to ruby

--- Parliament.Data.Api.FixedQuery/Controllers/ContactPointsController.cs
+++ Parliament.Data.Api.FixedQuery/Controllers/ContactPointsController.cs
@@ -14,38 +14,37 @@
         public Graph Index()
         {
             var queryString = @"
-PREFIX : <http://id.ukpds.org/schema/>
-
-CONSTRUCT {
-    ?contactPoint
-        a :ContactPoint ;
-        :email ?email ;
-        :phoneNumber ?phoneNumber ;
-        :faxNumber ?faxNumber ;
-        :contactPointHasPostalAddress ?postalAddress .
-    ?postalAddress a :PostalAddress ;
-        :postCode ?postCode ;
-        :addressLine1 ?addressLine1 ;
-        :addressLine2 ?addressLine2 ;
-        :addressLine3 ?addressLine3 ;
-        :addressLine4 ?addressLine4 ;
-        :addressLine5 ?addressLine5 .
-    }
-WHERE {
-    ?contactPoint a :ContactPoint ;
-    OPTIONAL{ ?contactPoint :email ?email . }
-    OPTIONAL{ ?contactPoint :phoneNumber ?phoneNumber . }
-    OPTIONAL{ ?contactPoint :faxNumber ?faxNumber . }
-    OPTIONAL{
-        ?contactPoint :contactPointHasPostalAddress ?postalAddress .
-        OPTIONAL{ ?postalAddress :postCode ?postCode . }
-       	OPTIONAL{ ?postalAddress :addressLine1 ?addressLine1 . }
-       	OPTIONAL{ ?postalAddress :addressLine2 ?addressLine2 . }
-       	OPTIONAL{ ?postalAddress :addressLine3 ?addressLine3 . }
-       	OPTIONAL{ ?postalAddress :addressLine4 ?addressLine4 . }
-       	OPTIONAL{ ?postalAddress :addressLine5 ?addressLine5 . }
-    }
-}
+PREFIX parl: <http://id.ukpds.org/schema/>
+     CONSTRUCT {
+           ?contactPoint
+               a parl:ContactPoint ;
+               parl:email ?email ;
+               parl:phoneNumber ?phoneNumber ;
+               parl:faxNumber ?faxNumber ;
+               parl:contactPointHasPostalAddress ?postalAddress .
+           ?postalAddress a parl:PostalAddress ;
+                       parl:postCode ?postCode ;
+               			   parl:addressLine1 ?addressLine1 ;
+               			   parl:addressLine2 ?addressLine2 ;
+               			   parl:addressLine3 ?addressLine3 ;
+               			   parl:addressLine4 ?addressLine4 ;
+               			   parl:addressLine5 ?addressLine5 .
+        }
+        WHERE {
+        	?contactPoint a parl:ContactPoint ;
+        	OPTIONAL{ ?contactPoint parl:email ?email . }
+        	OPTIONAL{ ?contactPoint parl:phoneNumber ?phoneNumber . }
+        	OPTIONAL{ ?contactPoint parl:faxNumber ?faxNumber . }
+        	OPTIONAL{
+                ?contactPoint parl:contactPointHasPostalAddress ?postalAddress .
+                OPTIONAL{ ?postalAddress parl:postCode ?postCode . }
+               	OPTIONAL{ ?postalAddress parl:addressLine1 ?addressLine1 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine2 ?addressLine2 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine3 ?addressLine3 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine4 ?addressLine4 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine5 ?addressLine5 . }
+          	}
+      }
 ";
 
             var query = new SparqlParameterizedString(queryString);
@@ -59,54 +58,55 @@ WHERE {
         public Graph ById(string id)
         {
             var queryString = @"
-PREFIX : <http://id.ukpds.org/schema/>
-
-CONSTRUCT {
-    ?contactPoint
-        a :ContactPoint ;
-        :email ?email ;
-        :phoneNumber ?phoneNumber ;
-        :faxNumber ?faxNumber ;
-        :contactPointHasPostalAddress ?postalAddress ;
-        :contactPointHasIncumbency ?incumbency .
-    ?incumbency
-        a :Incumbency ;
-        :incumbencyHasMember ?person .
-    ?postalAddress
-        a :PostalAddress ;
-        :postCode ?postCode ;
-        :addressLine1 ?addressLine1 ;
-        :addressLine2 ?addressLine2 ;
-        :addressLine3 ?addressLine3 ;
-        :addressLine4 ?addressLine4 ;
-        :addressLine5 ?addressLine5 .
-    ?person
-        a :Person ;
-        :personGivenName ?givenName ;
-    	:personFamilyName ?familyName .
-    }
-WHERE {
-	BIND(@id AS ?contactPoint )
-   	?contactPoint a :ContactPoint ;
-   	OPTIONAL{ ?contactPoint :email ?email . }
-   	OPTIONAL{ ?contactPoint :phoneNumber ?phoneNumber . }
-   	OPTIONAL{ ?contactPoint :faxNumber ?faxNumber . }
-   	OPTIONAL{
-        ?contactPoint :contactPointHasPostalAddress ?postalAddress .
-        OPTIONAL{ ?postalAddress :postCode ?postCode . }
-       	OPTIONAL{ ?postalAddress :addressLine1 ?addressLine1 . }
-       	OPTIONAL{ ?postalAddress :addressLine2 ?addressLine2 . }
-       	OPTIONAL{ ?postalAddress :addressLine3 ?addressLine3 . }
-       	OPTIONAL{ ?postalAddress :addressLine4 ?addressLine4 . }
-       	OPTIONAL{ ?postalAddress :addressLine5 ?addressLine5 . }
-       	}
-    OPTIONAL{
-	    ?contactPoint :contactPointHasIncumbency ?incumbency .
-        ?incumbency :incumbencyHasMember ?person .
-        OPTIONAL { ?person :personFamilyName ?familyName . }
-        OPTIONAL { ?person :personGivenName ?givenName . }
-    }
-}
+PREFIX parl: <http://id.ukpds.org/schema/>
+     CONSTRUCT {
+           ?contactPoint
+               a parl:ContactPoint ;
+               parl:email ?email ;
+               parl:phoneNumber ?phoneNumber ;
+               parl:faxNumber ?faxNumber ;
+               parl:contactPointHasPostalAddress ?postalAddress ;
+    		       parl:contactPointHasIncumbency ?incumbency .
+    		?incumbency
+        		a parl:Incumbency ;
+        		parl:incumbencyHasMember ?person .
+            ?postalAddress
+                a parl:PostalAddress ;
+                parl:postCode ?postCode ;
+                parl:addressLine1 ?addressLine1 ;
+                parl:addressLine2 ?addressLine2 ;
+                parl:addressLine3 ?addressLine3 ;
+                parl:addressLine4 ?addressLine4 ;
+                parl:addressLine5 ?addressLine5 .
+    		?person
+              a parl:Person ;
+              parl:personGivenName ?givenName ;
+        			parl:personFamilyName ?familyName ;
+              <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs .
+        }
+        WHERE {
+    		BIND(@id AS ?contactPoint )
+        	?contactPoint a parl:ContactPoint ;
+        	OPTIONAL{ ?contactPoint parl:email ?email . }
+        	OPTIONAL{ ?contactPoint parl:phoneNumber ?phoneNumber . }
+        	OPTIONAL{ ?contactPoint parl:faxNumber ?faxNumber . }
+        	OPTIONAL{
+                ?contactPoint parl:contactPointHasPostalAddress ?postalAddress .
+                OPTIONAL{ ?postalAddress parl:postCode ?postCode . }
+               	OPTIONAL{ ?postalAddress parl:addressLine1 ?addressLine1 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine2 ?addressLine2 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine3 ?addressLine3 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine4 ?addressLine4 . }
+               	OPTIONAL{ ?postalAddress parl:addressLine5 ?addressLine5 . }
+          	}
+            OPTIONAL{
+				      ?contactPoint parl:contactPointHasIncumbency ?incumbency .
+        		  ?incumbency parl:incumbencyHasMember ?person .
+        		  OPTIONAL { ?person parl:personFamilyName ?familyName . }
+        		  OPTIONAL { ?person parl:personGivenName ?givenName . }
+              OPTIONAL { ?person <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs } .
+            }
+      }
 ";
 
             var query = new SparqlParameterizedString(queryString);
