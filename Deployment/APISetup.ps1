5ae3d4dcf3dd1c4cf343a67f182e6a5526abb5dd
STAWIARSKI <stawiarskiw@parliament.uk>
2017-03-30 14:33:48 +0100

    api setup changes

--- Deployment/APISetup.ps1
+++ Deployment/APISetup.ps1
@@ -20,30 +20,38 @@ This script is for use as a part of deployment in VSTS only.
 
 Param(
     [Parameter(Mandatory=$true)] [string] $ClusterResourceGroupName,
-    [Parameter(Mandatory=$true)] [string] $APIManagementName,
     [Parameter(Mandatory=$true)] [string] $FixedQueryAPIName
 )
 
 $ErrorActionPreference = "Stop"
 
+$productTitle="Fixed Query"
+
 function Log([Parameter(Mandatory=$true)][string]$LogText){
     Write-Host ("{0} - {1}" -f (Get-Date -Format "HH:mm:ss.fff"), $LogText)
 }
 
-Log "Get API Management context"
-$management=New-AzureRmApiManagementContext -ResourceGroupName $ClusterResourceGroupName -ServiceName $APIManagementName
+Log "Retrives API Management"
+$apiManagement=Get-AzureRmApiManagement -ResourceGroupName $ClusterResourceGroupName
 
-Log "Access for Fixed Query API"
-$productFixedQuery=New-AzureRmApiManagementProduct -Context $management -Title "Parliament - Fixed Query API" -Description "For parliament use only." -ApprovalRequired $true -SubscriptionsLimit 1
-$api=New-AzureRmApiManagementApi -Context $management -Name "Fixed Query" -Description "All routes on Fixed Query API" -ServiceUrl "https://$FixedQueryAPIName.azurewebsites.net/" -Protocols @("https") -Path "/fixedquery"
-New-AzureRmApiManagementOperation -Context $management -ApiId $api.ApiId -Name "Fixed Query (catch all)" -Method "GET" -UrlTemplate "/"
-Add-AzureRmApiManagementApiToProduct -Context $management -ProductId $productFixedQuery.ProductId -ApiId $api.ApiId
+Log "Check if product already installed"
+$productFixedQuery=(Get-AzureRmApiManagementProduct -Context $management | Where-Object Title -Match $productTitle)
+if ($productFixedQuery -eq $null){
+    Log "Get API Management context"
+    $management=New-AzureRmApiManagementContext -ResourceGroupName $ClusterResourceGroupName -ServiceName $apiManagement.Name
+
+    Log "Access for Fixed Query API"
+    $productFixedQuery=New-AzureRmApiManagementProduct -Context $management -Title "Parliament - Fixed Query API" -Description "For parliament use only." -ApprovalRequired $true -SubscriptionsLimit 1
+    $api=New-AzureRmApiManagementApi -Context $management -Name "Fixed Query" -Description "All routes on Fixed Query API" -ServiceUrl "https://$FixedQueryAPIName.azurewebsites.net/" -Protocols @("https") -Path "/fixedquery"
+    New-AzureRmApiManagementOperation -Context $management -ApiId $api.ApiId -Name "Fixed Query (catch all)" -Method "GET" -UrlTemplate "/"
+    Add-AzureRmApiManagementApiToProduct -Context $management -ProductId $productFixedQuery.ProductId -ApiId $api.ApiId
+    Log "Add sparql endpoint api to Product ($productTitle)"
+    $api=Get-AzureRmApiManagementApi -Context $management | Where-Object Path -EQ "data"
+    Add-AzureRmApiManagementApiToProduct -Context $management -ProductId $productFixedQuery.ProductId -ApiId $api.ApiId
+}
 
 Log "Retrives subscription"
 $subscription=Get-AzureRmApiManagementSubscription -Context $management -ProductId $productFixedQuery.ProductId
-
-Log "Retrives IP address"
-$apiManagement=Get-AzureRmApiManagement -ResourceGroupName $ClusterResourceGroupName -Name $APIManagementName
 
 Log "Setting variables to use during deployment"
 Write-Host "##vso[task.setvariable variable=SubscriptionKey]$($subscription.PrimaryKey)"
