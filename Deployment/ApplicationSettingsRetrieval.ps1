a797c621c57a4a2865903179ed6d2fb6bf2ce1a1
hellikopter <hellikopter@users.noreply.github.com>
2017-06-05 10:05:32 +0100

    settings retrieval during deployment

--- Deployment/ApplicationSettingsRetrieval.ps1
+++ Deployment/ApplicationSettingsRetrieval.ps1
@@ -1,25 +1,19 @@
 <#
 .SYNOPSIS
-Get instrumentation key for use by API app.
+Get settings for API app.
 
 .DESCRIPTION
-Retrieves value of Application Insights' Instrumentation Key and sets task variable.
+Retrieves value of Subscription Key and IP of API Management and sets task variables.
 
-.PARAMETER OperationsResourceGroupName
-Name of the Resource Group where the Application Insights is.
-
-.PARAMETER ApplicationInsightsName
-Name of the Application Insights.
+.PARAMETER APIResourceGroupName
+Name of the Resource Group where the API Management is.
 
 .NOTES
 This script is for use as a part of deployment in VSTS only.
 #>
 
 Param(
-    [Parameter(Mandatory=$true)] [string] $OperationsResourceGroupName,
-    [Parameter(Mandatory=$true)] [string] $ApplicationInsightsName,
-	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName,
-	[Parameter(Mandatory=$true)] [string] $APIManagementName
+	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName
 )
 $ErrorActionPreference = "Stop"
 
@@ -27,21 +21,15 @@ function Log([Parameter(Mandatory=$true)][string]$LogText){
     Write-Host ("{0} - {1}" -f (Get-Date -Format "HH:mm:ss.fff"), $LogText)
 }
 
-Log "Getting Instrumentation Key"
-$properties=Get-AzureRmResource -ResourceGroupName $OperationsResourceGroupName -ResourceName $ApplicationInsightsName -ExpandProperties | Select-Object Properties -ExpandProperty Properties
-
+Log "Get API Management"
+$apiManagement=Get-AzureRmApiManagement -ResourceGroupName $APIResourceGroupName
 Log "Get API Management context"
-$management=New-AzureRmApiManagementContext -ResourceGroupName $APIResourceGroupName -ServiceName $APIManagementName
+$management=New-AzureRmApiManagementContext -ResourceGroupName $APIResourceGroupName -ServiceName $apiManagement.Name
 Log "Retrives subscription"
 $apiProduct=Get-AzureRmApiManagementProduct -Context $management -Title "Parliament - Fixed Query"
 $subscription=Get-AzureRmApiManagementSubscription -Context $management -ProductId $apiProduct.ProductId
 
-Log "Retrives API Management"
-$apiManagement=Get-AzureRmApiManagement -ResourceGroupName $APIResourceGroupName
-
 Log "Setting variables to use during deployment"
-Log "Instrumentation Key: $($properties.InstrumentationKey)"
-Write-Host "##vso[task.setvariable variable=ApplicationInsightsInstrumentationKey]$($properties.InstrumentationKey)"
 Write-Host "##vso[task.setvariable variable=SubscriptionKeyFixedQuery]$($subscription.PrimaryKey)"
 Write-Host "##vso[task.setvariable variable=APIManagementIP]$($apiManagement.StaticIPs[0])"
 
