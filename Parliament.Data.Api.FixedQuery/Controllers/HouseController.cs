f2211bc4cef534c9a16f233163219dcdb8d6c7c7
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-07-05 15:01:07 +0100

    Add image to HousePartyMembers

--- Parliament.Data.Api.FixedQuery/Controllers/HouseController.cs
+++ Parliament.Data.Api.FixedQuery/Controllers/HouseController.cs
@@ -810,6 +810,7 @@ CONSTRUCT {
         a :Person ;
         :personGivenName ?givenName ;
         :personFamilyName ?familyName ;
+        :personHasImage ?image ;
         <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs ;
         <http://example.com/A5EE13ABE03C4D3A8F1A274F57097B6C> ?listAs ;
         :partyMemberHasPartyMembership ?partyMembership ;
@@ -859,6 +860,7 @@ WHERE {
                 :incumbencyHasMember ?person .
             OPTIONAL { ?person :personGivenName ?givenName . }
             OPTIONAL { ?person :personFamilyName ?familyName . }
+            OPTIONAL { ?person :personHasImage ?image . }
             OPTIONAL { ?person <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs } .
             ?person <http://example.com/A5EE13ABE03C4D3A8F1A274F57097B6C> ?listAs .
             {
