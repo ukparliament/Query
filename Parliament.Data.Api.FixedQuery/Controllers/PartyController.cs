d69f30a53b67a8b40e0d786af1c561700fb8fa9d
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-07-05 15:23:51 +0100

    Add image to PartyMembersByInitial

--- Parliament.Data.Api.FixedQuery/Controllers/PartyController.cs
+++ Parliament.Data.Api.FixedQuery/Controllers/PartyController.cs
@@ -530,6 +530,7 @@ CONSTRUCT {
         a :Person ;
         :personGivenName ?givenName ;
         :personFamilyName ?familyName ;
+        :personHasImage ?image ;
         <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs ;
         <http://example.com/A5EE13ABE03C4D3A8F1A274F57097B6C> ?listAs ;
         :memberHasIncumbency ?incumbency ;
@@ -565,6 +566,7 @@ WHERE {
             OPTIONAL { ?partyMembership :partyMembershipEndDate ?endDate . }
             OPTIONAL { ?person :personGivenName ?givenName . }
             OPTIONAL { ?person :personFamilyName ?familyName . }
+            OPTIONAL { ?person :personHasImage ?image . }
             OPTIONAL { ?person <http://example.com/F31CBD81AD8343898B49DC65743F0BDF> ?displayAs } .
             ?person <http://example.com/A5EE13ABE03C4D3A8F1A274F57097B6C> ?listAs ;
                 	:memberHasIncumbency ?incumbency .
