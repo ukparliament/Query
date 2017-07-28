5469b0ffd64c6694de529e2591ef4fc4a581ef0f
Christopher Alcock <alcockc@parliament.uk>
2017-07-28 10:41:18 +0000

    sets default to n-triples in line with previous API

--- Parliament.Data.Api.FixedQuery/Global.asax.cs
+++ Parliament.Data.Api.FixedQuery/Global.asax.cs
@@ -20,6 +20,7 @@
 
             config.Formatters.Clear();
 
+            config.Formatters.Add(new GraphFormatter(new UriPathExtensionMapping("nt", "application/n-triples")));
             config.Formatters.Add(new GraphFormatter(new UriPathExtensionMapping("ttl", "text/turtle")));
             config.Formatters.Add(new GraphFormatter(new UriPathExtensionMapping("json", "application/rdf+json")));
             config.Formatters.Add(new GraphFormatter(new UriPathExtensionMapping("xml", "application/rdf+xml")));
@@ -34,7 +35,7 @@
             config.Formatters.Add(new GraphFormatter(new RequestHeaderMapping("Accept", "text/turtle", StringComparison.Ordinal, false, "text/turtle")));
             config.Formatters.Add(new GraphFormatter(new RequestHeaderMapping("Accept", "application/rdf+json", StringComparison.Ordinal, false, "application/rdf+json")));
             config.Formatters.Add(new GraphFormatter(new RequestHeaderMapping("Accept", "application/json", StringComparison.Ordinal, false, "application/rdf+json")));
-
+            config.Formatters.Add(new GraphFormatter(new RequestHeaderMapping("Accept", "application/n-triples", StringComparison.Ordinal, false, "application/n-triples")));
             config.Formatters.Add(new GraphFormatter(new RequestHeaderMapping("Accept", "text/html", StringComparison.Ordinal, false, "text/html")));
 
 
@@ -42,6 +43,7 @@
             config.Formatters.Add(new HttpErrorXmlFormatter());
 
             config.Routes.MapHttpRoute("Index", "", new { controller = "Help" });
+            config.Routes.IgnoreRoute("css", "resourceStyle.css");
 
             config.Routes.MapHttpRoute("WithExtension", "{action}.{ext}", new { controller = "FixedQuery" });
             config.Routes.MapHttpRoute("WithoutExtension", "{action}", new { controller = "FixedQuery" });
