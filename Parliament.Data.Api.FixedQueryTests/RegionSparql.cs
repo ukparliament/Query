8c3fd9a2fc90a2f481875684a6458cc59c9c02ba
christopheralcock <christopher.alcock@gmail.com>
2017-08-10 17:51:57 +0100

    adds region tests and updates proj file

---
+++ Parliament.Data.Api.FixedQueryTests/RegionSparql.cs
@@ -0,0 +1,32 @@
+﻿namespace Parliament.Data.Api.FixedQuery.Controllers.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Parliament.Data.Api.FixedQuery.Controllers;
+    using Parliament.Data.Api.FixedQueryTests;
+
+    [TestClass()]
+    [TestCategory("Region")]
+    [TestCategory("Sparql")]
+    public class RegionSparql : SparqlValidator
+    {
+        private FixedQueryController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            controller = new FixedQueryController();
+        }
+
+        [TestMethod()]
+        public void RegionIndexSparql()
+        {
+            ValidateSparql(() => controller.region_index());
+        }
+
+        [TestMethod()]
+        public void RegionConstituenciesSparql()
+        {
+            ValidateSparql(() => controller.region_constituencies(string.Empty));
+        }
+    }
+}
