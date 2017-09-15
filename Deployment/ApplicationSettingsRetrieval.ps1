bc503bee3148786a918e0e60e62935df9b2321e0
hellikopter <hellikopter@users.noreply.github.com>
2017-09-15 15:14:24 +0100

    api names changes

--- Deployment/ApplicationSettingsRetrieval.ps1
+++ Deployment/ApplicationSettingsRetrieval.ps1
@@ -13,7 +13,9 @@ This script is for use as a part of deployment in VSTS only.
 #>
 
 Param(
-	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName
+	[Parameter(Mandatory=$true)] [string] $APIResourceGroupName,
+	[Parameter(Mandatory=$true)] [string] $APIManagementName,
+	[Parameter(Mandatory=$true)] [string] $APIPrefix
 )
 $ErrorActionPreference = "Stop"
 
@@ -21,12 +23,10 @@ function Log([Parameter(Mandatory=$true)][string]$LogText){
     Write-Host ("{0} - {1}" -f (Get-Date -Format "HH:mm:ss.fff"), $LogText)
 }
 
-Log "Get API Management"
-$apiManagement=Get-AzureRmApiManagement -ResourceGroupName $APIResourceGroupName
 Log "Get API Management context"
-$management=New-AzureRmApiManagementContext -ResourceGroupName $APIResourceGroupName -ServiceName $apiManagement.Name
+$management=New-AzureRmApiManagementContext -ResourceGroupName $APIResourceGroupName -ServiceName $APIManagementName
 Log "Retrives subscription"
-$apiProduct=Get-AzureRmApiManagementProduct -Context $management -Title "Parliament - Fixed Query"
+$apiProduct=Get-AzureRmApiManagementProduct -Context $management -Title "$APIPrefix - Parliament [Fixed Query]"
 $subscription=Get-AzureRmApiManagementSubscription -Context $management -ProductId $apiProduct.ProductId
 
 Log "Setting variables to use during deployment"
