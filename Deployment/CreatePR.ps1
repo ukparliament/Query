63723032a7aa5363dc4f20d8686c6bcfafc9bec4
Samu Lang <langsamu@users.noreply.github.com>
2018-08-24 20:22:31 +0100

    Update CreatePR.ps1

--- Deployment/CreatePR.ps1
+++ Deployment/CreatePR.ps1
@@ -6,13 +6,9 @@ param (
 $parsedSourceBranch = $branch | ConvertFrom-String -TemplateContent "refs/pull/{PR:pr}/merge"
 $newBranch = "GitHubPullRequest/$($parsedSourceBranch.PR)"
 
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$env:SYSTEM_ACCESSTOKEN"))
-
-$base64AuthInfo
-
-$uri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)$env:SYSTEM_TEAMPROJECTID/_apis/git/repositories/Query/pullrequests?api-version=4.1"
+$uri = "$($env:SYSTEM_TEAMFOUNDATIONCOLLECTIONURI)/$($env:SYSTEM_TEAMPROJECT)/_apis/git/repositories/Query/pullrequests?api-version=4.1"
 
 $body = @{targetRefName = "refs/heads/master"; sourceRefName = "refs/heads/$($newBranch)"; title = $newBranch} | ConvertTo-Json
 
-Invoke-RestMethod -Uri $uri -Method Post -ContentType "application/json" -Headers @{Authorization = "Basic $base64AuthInfo"} -Body $body
+Invoke-RestMethod -Uri $uri -Method Post -ContentType "application/json" -Headers @{Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"} -Body $body
 
