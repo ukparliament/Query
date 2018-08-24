271a88785dfbd2cc03fa3c4a90906e4b581ee476
Samu Lang <langsamu@users.noreply.github.com>
2018-08-24 19:49:44 +0100

    Update CreatePR.ps1

--- Deployment/CreatePR.ps1
+++ Deployment/CreatePR.ps1
@@ -8,6 +8,9 @@ $newBranch = "GitHubPullRequest/$($parsedSourceBranch.PR)"
 
 $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($token)"))
 
-$uri = "https://data-parliament.visualstudio.com/Platform/_apis/git/repositories/Query/pullrequests?api-version=4.1
+$uri = "https://data-parliament.visualstudio.com/Platform/_apis/git/repositories/Query/pullrequests?api-version=4.1"
 
-$body = @{targetRefName = "refs/heads/master"}
+$body = @{targetRefName = "refs/heads/master"; sourceRefName = "refs/heads/$($newBranch)"; title = $newBranch} | ConvertTo-Json
+
+Invoke-RestMethod -Uri $uri -Method Post -ContentType "application/json" -Headers @{Authorization = "Basic $($base64AuthInfo)"} -Body $body
+
